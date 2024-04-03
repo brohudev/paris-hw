449d4e4c61f804cc991a19c3e3dcdb1f2d27990b
brohudev <hitarth.thanki9006@gmail.com>
2024-04-02 19:21:31 -0500

    get rid of all the complexity, baby steps from scratch

--- hw2/client.cpp
+++ hw2/client.cpp
@@ -1,119 +1,42 @@
 #include <iostream>
-#include <cstring>
-#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <fstream>
-using namespace std;
+#include <unistd.h>
+#include <cstring>
 
-#define PORT_NO 32768
-#define IP_ADDRESS "127.0.0.1"
-#define BUFFER_SIZE 1024 //change in the other file as well.
-
-class SocketClient {
-    private:
-        int socket_fd;
-
-    public:
-        SocketClient(const char*, int);
-        ~SocketClient();
-        void sendRequest(const string&);
-        void receiveFile(const string&);
-};
-
-SocketClient::SocketClient(const char* ip_address, int port_no){
-        // Create socket
-    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_fd == -1) {
-        std::cerr << "Error creating socket\n";
-        return;
+int main() {
+    // Create socket
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == -1) {
+        std::cerr << "Error: Could not create socket\n";
+        return 1;
     }
 
     // Connect to server
     struct sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(port_no);
-    serverAddr.sin_addr.s_addr = inet_addr(ip_address);
+    serverAddr.sin_port = htons(8081);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    if (connect(socket_fd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
-        std::cerr << "Error connecting to server\n";
-        close(socket_fd);
-        return;
+    if (connect(clientSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+        std::cerr << "Error: Connection failed\n";
+        return 1;
     }
 
-    std::cout << "Connected to server\n";
-}
-SocketClient::~SocketClient(){
+    // Send message to server
+    std::cout << "Enter message to send to server: ";
+    std::string message;
+    std::getline(std::cin, message);
+
+    send(clientSocket, message.c_str(), message.size(), 0);
+    std::cout << "Message sent to server\n";
+
     // Close socket
-    close(socket_fd);
-}
-void SocketClient::sendRequest(const std::string& request) {
-    int makeRequest = send(socket_fd, request.c_str(), request.size(), 0);
-    
-    if (makeRequest == -1) {
-        std::cerr << "Error sending request to server\n";
-        return;
-    }
-}
-void SocketClient::receiveFile(const std::string& filename) {
-    char buffer[BUFFER_SIZE];
-    int bytesReceived;
-    
-    std::ofstream outfile(filename, std::ios::binary); //open file
-    if (!outfile) {
-        std::cerr << "Error opening file for writing\n";
-        return;
-    }
+    close(clientSocket);
 
-    while ((bytesReceived = recv(socket_fd, buffer, sizeof(buffer), 0)) > 0) { //write received bytes to file
-        outfile.write(buffer, bytesReceived);
-    }
-
-    if (bytesReceived == -1) {
-        outfile.close();
-        if (remove(filename.c_str()) != 0) { //clean up the file of any erroneous bits
-            std::cerr << "Error cleaning up file\n";
-        }
-
-        std::cerr << "Error receiving file from server\n";
-        return;
-    }
-
-    // Close file
-    outfile.close();
-    std::cout << "File received: " << filename << " (" << bytesReceived << " bytes)\n";
-}
-int main() {
-    SocketClient client(IP_ADDRESS, PORT_NO); //wrap everything to clean up main 
-    cout << "Client started on port "<< PORT_NO << endl;
-
-    while (true) {
-        string input;
-        cout << "Enter your request: ";
-        getline(std::cin, input);
-        //make the code more readable :D
-        string command = input.substr(0, input.find_first_of(" "));
-
-        if (command == "get") {
-            string fileName = input.substr(input.find_first_of(" ")+1);
-            cout << "attempting to get file: " << fileName <<endl;
-
-            client.sendRequest(fileName); //only send in the name bit of the input
-
-            client.receiveFile(fileName);
-        } else if (input == "exit") {
-            client.sendRequest("terminate");
-            return 0; //send message and close
-        } else if (input == "terminate") {
-            return 0; //close dont send message
-        } else {
-            std::cout << "Invalid request. Please try again.\n";
-        }
-    }
     return 0;
 }
-
 
 /*
 1. Initialize socket for client
--- hw2/server.cpp
+++ hw2/server.cpp
@@ -1,167 +1,74 @@
 #include <iostream>
-#include <string>
-#include <cstring>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <fstream>
-#include <sys/wait.h>
-using namespace std;
 
-#define PORT 32768
-#define IP_ADDRESS "127.0.0.1" //idk why this is here even though it does nothing
-#define BUFFER_SIZE 1024 //change in the other file as well if you want a diff size
-#define REPOSITORY_DIRECTORY "~/Repository/"
-
-class SocketServer {
-private:
-    int serverSocket;
-
-public:
-    SocketServer(const char*, int);
-    ~SocketServer();
-    void handleClient(int clientSocket);
-    void listenForConnection();
-};
-
-SocketServer::SocketServer(const char* ip_address, int port_no) {
+int main() {
     // Create socket
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1) {
-        std::cerr << "Error creating socket\n";
-        return;
+        std::cerr << "Error: Could not create socket\n";
+        return 1;
     }
 
-    // Bind to port
+    // Bind socket to port 8081
     struct sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htons(port_no); // Port number
+    serverAddr.sin_port = htons(8081);
+
     if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
-        std::cerr << "Error binding to port\n";
-        close(serverSocket);
-        return;
+        std::cerr << "Error: Could not bind to port\n";
+        return 1;
     }
 
-    // Listen for incoming connections
-    if (listen(serverSocket, 5) == -1) {
-        std::cerr << "Error listening for connections\n";
-        close(serverSocket);
-        return;
+    // Listen
+    if (listen(serverSocket, 10) == -1) {
+        std::cerr << "Error: Could not listen on socket\n";
+        return 1;
     }
 
-    std::cout << "listening on port no " << PORT << std::endl;
-}
+    std::cout << "Server listening on port 8081...\n";
 
-SocketServer::~SocketServer() {
-    close(serverSocket);
-}
-
-void SocketServer::handleClient(int clientSocket) {
-    char buffer[BUFFER_SIZE];
-    int bytesReceived;
-
-    std::cout << "got a client request.." << std::endl;
+    // Accept incoming connections
+    struct sockaddr_in clientAddr;
+    socklen_t clientLen = sizeof(clientAddr);
+    int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &clientLen);
+    if (clientSocket == -1) {
+        std::cerr << "Error: Could not accept incoming connection\n";
+        return 1;
+    }
 
     // Receive message from client
-    while ((bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
-        buffer[bytesReceived] = '\0';
-        std::string message(buffer);
+    char buffer[1024] = {0};
+    int valread = read(clientSocket, buffer, 1024);
+    std::cout << "Received message from client: " << buffer << std::endl;
 
-        if (message.find("get ") == 0) {
-            // Extract filename
-            std::string filename = message.substr(4);
-            std::string filepath = std::string(REPOSITORY_DIRECTORY) + filename;
-
-            // Check if file exists
-            std::ifstream file(filepath, std::ios::binary);
-            if (file) {
-                // File exists
-                std::cout << "File found: " << filename << std::endl;
-
-                // Send file contents to client
-                while (file.read(buffer, sizeof(buffer))) {
-                    send(clientSocket, buffer, file.gcount(), 0);
-                }
-
-                // Print message indicating file sent and its size
-                std::cout << "File sent: " << filename << " (" << file.tellg() << " bytes)\n";
-
-                // Close file
-                file.close();
-            } else {
-                // File doesn't exist
-                std::cout << "File not found: " << filename << std::endl;
-
-                // Send message to client indicating file is missing
-                send(clientSocket, "FileNotFound", strlen("FileNotFound"), 0);
-            }
-        } else if (message == "terminate") {
-            // Send "Goodbye!" message to client
-            send(clientSocket, "Goodbye!", strlen("Goodbye!"), 0);
-
-            // Terminate client connection
-            break;
-        }
-    }
-
-    if (bytesReceived == 0) {
-        std::cout << "Client disconnected\n";
-    } else {
-        std::cerr << "Error receiving message from client\n";
-    }
-
-    // close(clientSocket);
-}
-
-void SocketServer::listenForConnection() {
-    // Accept incoming connection
-    struct sockaddr_in clientAddr;
-    socklen_t clientAddrLen = sizeof(clientAddr);
-    int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &clientAddrLen);
-    if (clientSocket == -1) {
-        std::cout << "Error accepting connection\n";
-        return;
-    }
-
-    // Handle client request
-    handleClient(clientSocket);
-}
-
-int main() {
-    SocketServer server(IP_ADDRESS, PORT);
-
-    std::cout << "Server listening on port " << PORT << std::endl;
-
-    // Loop indefinitely
-    while (true) {
-        server.listenForConnection();
-    }
+    // Close sockets
+    close(clientSocket);
+    close(serverSocket);
 
     return 0;
 }
+
 
 /*
 1. Initialize socket for server
 2. Bind socket to server address
 3. Listen for incoming connections
 4. Loop indefinitely:
-   5. Accept incoming connection
-   6. Fork a child process to handle client
-   7. Inside child process:
-      8. Loop indefinitely:
-         9. Receive message from client
-         10. If message is "get filename":
-             11. Check if file exists in Repository directory
-             12. If file exists:
-                 13. Open file and read its contents
-                 14. Send file contents to client
-                 15. Print message indicating file sent and its size
-             16. Else:
-                 17. Send message to client indicating file is missing
-         18. Else if message is "terminate":
-             19. Send "Goodbye!" message to client
-             20. Terminate child process
+    5. Accept incoming connection
+    8. Loop indefinitely:
+        9. Receive message from client
+        10. If message is "get filename":
+            11. Check if file exists in Repository directory
+            12. If file exists:
+                13. send the file size and name as a message. 
+                15. Print message indicating file sent and its size
+            16. Else:
+                17. Send message to client indicating file is missing
+        18. Else if message is "terminate":
+            19. Send "Goodbye!" message to client
+            20. Terminate child process
 21. Close server socket
-
 */
